4a28bc1e0813dcd7ee3029eaf074370f1f0733a2
sainudheenp <sainudheen2003@gmail.com>
2024-10-03 21:54:33 +0530

    last

---
+++ SumArray.js
@@ -0,0 +1,15 @@
+// Write a function to find sum of all elements in an array
+
+let sum = (arr) => {
+  //   SplitedArray = arr.split(",");
+  sum = 0;
+
+  for (i = 0; i < 5; i++) {
+    sum = arr[i] + sum;
+  }
+
+  console.log(sum);
+};
+
+let sampleArray = [1, 2, 3];
+sum(sampleArray);
