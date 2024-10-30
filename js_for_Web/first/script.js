57d0731c1a7cfcbcf15e4c85de0e8a17e08a3ab9
sainudheenp <sainudheen2003@gmail.com>
2024-10-30 16:00:01 +0530

    .

---
+++ js_for_Web/first/script.js
@@ -0,0 +1,28 @@
+let radius = 10;
+const pi = 3.14 ;
+
+
+
+// console.log(10/2)
+// let results = radius %3
+// let results = pi * radius**2 
+// console.log(results)
+
+ let likes = 0 ;
+ likes ++;
+ console.log(likes)
+ likes +=1;
+ console.log(likes)
+ likes -=2;
+ console.log(likes)
+
+
+// concatenation string
+
+let conc = "The Blog has "+ likes + " Likes "
+console.log(conc)
+
+//  templates string
+
+let tem =  `The Blog has ${likes} likes`
+console.log(tem )
