30aa6c7f748c10b25c321e4c581a23a1d31b896a
sainudheenp <sainudheen2003@gmail.com>
2024-10-31 13:38:58 +0530

    error handling

--- js_for_Web/first/script.js
+++ js_for_Web/first/script.js
@@ -1,5 +1,5 @@
-let radius = 10;
-const pi = 3.14 ;
+// let radius = 10;
+// const pi = 3.14 ;
 
 
 
@@ -8,21 +8,34 @@ const pi = 3.14 ;
 // let results = pi * radius**2 
 // console.log(results)
 
- let likes = 0 ;
- likes ++;
- console.log(likes)
- likes +=1;
- console.log(likes)
- likes -=2;
- console.log(likes)
+//  let likes = 0 ;
+//  likes ++;
+//  console.log(likes)
+//  likes +=1;
+//  console.log(likes)
+//  likes -=2;
+//  console.log(likes)
 
 
 // concatenation string
 
-let conc = "The Blog has "+ likes + " Likes "
-console.log(conc)
+// let conc = "The Blog has "+ likes + " Likes "
+// console.log(conc)
 
 //  templates string
 
-let tem =  `The Blog has ${likes} likes`
-console.log(tem )
+// let tem =  `The Blog has ${likes} likes`
+// console.log(tem )
+
+let ni = ["n", "i","n","j","a"]
+// join
+console.log(ni.join("_"))
+
+console.log(ni.indexOf("i"))
+
+// concat
+console.log(ni.concat(["Y","T"]))
+
+// push
+console.log(ni.push(["hi"]))
+console.log(ni.pop("n" ))
