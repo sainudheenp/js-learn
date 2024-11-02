e8b5dce2b453bdf21f1652aff28a17485fa0dd78
sainudheenp <sainudheen2003@gmail.com>
2024-11-02 10:05:41 +0530

    color class change

--- js_for_Web/sandbox.js
+++ js_for_Web/sandbox.js
@@ -2,4 +2,7 @@ const link = document.querySelector("a")
 link.innerHTML = "Vonnue.com"
 link.setAttribute("href","https://www.vonnue.com")
 
+p = document.querySelector('p')
+p.setAttribute("class" , "blue_p")
+
 console.log(link)
