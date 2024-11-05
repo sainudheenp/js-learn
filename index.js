2b2a71773970a1edd398bc9fb9a1f21c8b346fe4
sainudheenp <sainudheen2003@gmail.com>
2024-11-05 12:09:25 +0530

    .

--- index.js
+++ index.js
@@ -24,15 +24,69 @@ console.log("last letter:", namee[namee.length - 1])
 
 
 
+// for loops
+
+body = document.querySelector("body");
+let i =0;
+const forArray = ['s', "sf", "k"]
+for ( i = 0; i < forArray.length; i++) {
+console.log(forArray[i]);
+body.innerHTML+=`<div>${forArray[i]}</div>`
+}
 
 
 
+// while
+// let i =0;
+body = document.querySelector('body')
+while(i < forArray.length){
+    console.log("Hi am while loop")
+    body.innerHTML += `<div>${forArray[i]}</div>`
+    i++;
+}
+
+// do while 
+do{
+    console.log("hi am do while")
+
+}
+while(0>1)
+
+
+const password = "19244dd4@"
+if (password.length >=8 && password.includes('@')){
+    console.log("Strong password!!")
+}
+else{
+    console.log("pd Not secure")
+}
+
+
+if( false){
+    console.log("false")
+}
+else{
+    console.log("true")
+}
+
+
+// student mark
+let marks =[90,39,100,0,33]
+for(let i=0;i< marks.length;i++){
+    console.log("Your Score: ",marks[i])
+    if(marks[i]===0){
+        continue;//exit current iteration
+
+    }
+
+    if(marks[i]===100){
+        console.log("congrats, you got top score! ");
+        break;
+    }
 
 
 
-
-
-
+}
 
 
 
@@ -74,9 +128,9 @@ function worldBlanks(myNoun, MyAdjective, myVerb, myAdverb) {
     var result = `The ${MyAdjective} ${myNoun} ${myVerb} to the store ${myAdverb}. `
     return result;
 }
-console.log(worldBlanks("dog","big","ran","quickly"))
+console.log(worldBlanks("dog", "big", "ran", "quickly"))
 
 
 
-let ourArray= ["john",32]
-let myArray=["sin",44]
+let ourArray = ["john", 32]
+let myArray = ["sin", 44]
