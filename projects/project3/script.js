de102db74b5b6e3080a352a2a550e44789d351ce
sainudheenp <sainudheen2003@gmail.com>
2024-11-12 13:25:58 +0530

    project3 done

--- projects/project3/script.js
+++ projects/project3/script.js
@@ -3,13 +3,16 @@
 const  accordion = document.getElementsByClassName('content-container')
 console.log(accordion)
 
-// accordion.forEach(ac => {
-    
+// accordion.forEach( item => {
+//     item.addEventListener("click", ()=>{
+//         item.classList.toggle('active')
+//     })
 // });
 
 for(i=0 ; i < accordion.length; i++){
     accordion[i].addEventListener('click' , function(){
         this.classList.toggle('active');
-        
+        console.log("hle",this)
+
     })
 }
