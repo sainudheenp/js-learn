e73bbae03d5cca128a585b04d24571812c1e4673
sainudheenp <sainudheen2003@gmail.com>
2024-11-15 13:31:07 +0530

    arraymax

--- neet/arraymax.js
+++ neet/arraymax.js
@@ -1,4 +1,4 @@
-// find max value in array
+// find max value in array v1 works
 
 // function maxVal(arr){
 //     console.log(arr.sort((a,b)=>a-b)[arr.length-1])
@@ -7,19 +7,41 @@
 
 
 
+// v2 works
+// function maxVal(arr) {
+//     let max = 0;
+//     for (i = 1; i <= arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i]
+//         }
+//         else {
+//             max = max
+//         }
+//     }
+//     console.log(max)
+// }
+// maxVal([2, 4, 12, 3, 5, 1, 9])
+
 
 function maxVal(arr) {
     let max = 0;
     for (i = 1; i <= arr.length; i++) {
-        if ( arr[i] > max) {
+        if (arr[i] > max) {
             max = arr[i]
         }
-        else{
-            max=max
+        else {
+            max = max
         }
     }
-    console.log(max)
+    return max;
 }
 
+function test(input, exsum) {
+    input.forEach((inp, idx) => {
+      return  maxVal(inp) == exsum[idx] ? console.log("Test Passed") : console.log("Test Failed")
+    });
 
-maxVal([2, 4, 12,3,5,1,9])
+}
+let input = [[1, 2, 3, 4],[2, 4, 5, 6],[6, 5, 7, 8]]
+let exsum = [4, 6, 8]
+test(input, exsum)
