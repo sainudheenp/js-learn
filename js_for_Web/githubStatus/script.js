f28d54abba367f95635c2751b9940564697fd0e4
sainudheenp <sainudheen2003@gmail.com>
2024-11-17 21:52:26 +0530

    lastt

--- js_for_Web/githubStatus/script.js
+++ js_for_Web/githubStatus/script.js
@@ -1,31 +1,30 @@
 document.getElementById("form").addEventListener("submit", (ev) => {
   ev.preventDefault();
 
-  let username = document.getElementById("username").value
+  let username = document.getElementById("username").value;
 
   console.log(username);
   const url = `https://api.github.com/users/${username}`;
 
+  let ul = document.querySelector("ul");
 
   fetch(url)
-  .then(response => {
-    if (! response.ok) {
-      throw new Error("User not found");
-    }
-    return response.json();
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("User not found");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.avatar_url);
+      let li = `Name : ${data.name}`;
 
-  })
-  .then(data => {
-    console.log(data);
-    console.log(data.avatar_url);
-
-  }).catch(error =>{
-    console.log("Error mahn :",error)
-  })
-
-
+      ul.append(data);
+    })
+    .catch((error) => {
+      console.log("Error mahn :", error);
+    });
 });
 
 // let username = "sainudheenp";
-
-
