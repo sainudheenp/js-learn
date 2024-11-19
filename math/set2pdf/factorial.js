a5a57adc113babdc978858311aef9ba4a1ab45ec
sainudheenp <sainudheen2003@gmail.com>
2024-11-19 18:56:24 +0530

    Recursive learn

--- math/set2pdf/factorial.js
+++ math/set2pdf/factorial.js
@@ -1,9 +1,28 @@
+// // factorial of a number using recursive methode
+// function fact(num) {
+//   let fact = 1;
+//   for (i = 1; i <= num; i++) {
+//     fact =fact * i
+//   }
+//   console.log(fact)
+// }
+// fact(5);
+
 // factorial of a number using recursive methode
+
 function fact(num) {
-  let fact = 1;
-  for (i = 1; i <= num; i++) {
-    fact =fact * i
+  if (num == 0) {
+    return 1;
+  } else {
+    return num * fact(num - 1);
   }
-  console.log(fact)
 }
-fact(5);
+
+console.log(fact(10));
+
+//num * fact(num-1) 5* fact4)      5*24       120
+//                  4* fact(3)   (4*6)        24
+//                  3* fact(2)   (3*2)  return 6
+//                  2* fact(1)   (2*1) return 2
+//                  1* fact(0)  (1*1) return 1
+//                   retrun 1
