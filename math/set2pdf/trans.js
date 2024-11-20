7e2f4c3140906eded7a57bac805643ca700816e6
sainudheenp <sainudheen2003@gmail.com>
2024-11-20 11:13:40 +0530

    array compare failed without converting into strings

---
+++ math/set2pdf/trans.js
@@ -0,0 +1,44 @@
+function transpose(arr) {
+    arr.forEach((iarray, i) => {
+        iarray.forEach((item, j) => {
+            transMatrix[j][i] = item
+        })
+    })
+    return transMatrix
+}
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+let transMatrix = [[], [], []];
+
+let inputs = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[1, 2, 3], [4, 5, 6], [7, 8, 9]]]
+let outputs = [[[1, 4, 7], [2, 5, 8], [3, 6, 9]], [[1, 4, 7], [2, 5, 8], [3, 6, 9]], [[1, 4, 7], [2, 5, 8], [3, 6, 9]]]
+
+
+let test = (inputs, outputs) => {
+    inputs.forEach((arr, i) => {
+        // console.log(arr)
+        // console.log("ret", transpose(arr))
+        // console.log("exp", outputs[i])
+
+        if (transpose(arr) == outputs[i]) {
+            console.log("Test Passed")
+        }
+        else {
+            console.log("Test Failed")
+        }
+    })
+    
+}
+
+
+test(inputs, outputs)
+
+
+
+
+
+
+// transpose(matrix);
+// console.log(...matrix)
+// console.log(...transMatrix)
+
+
