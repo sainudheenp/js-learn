c568420153f7163f4bef90380624f0ac865c04ea
sainudheenp <sainudheen2003@gmail.com>
2024-11-23 11:23:35 +0530

    xl

--- math/set2pdf/q15-dectonbin.js
+++ math/set2pdf/q15-dectonbin.js
@@ -1,3 +1,28 @@
-function toBin(number) {
-    let rmArray = [];
-    for (i = 0; i < number; i++) {
+// decimal  to bin 
+
+
+function dectobin(dec) {
+    let remarray = []
+    let val = 0;
+    let rem = 0;
+    do {
+
+        rem = Math.floor((dec % 2))
+        remarray.push(rem)
+        // console.log(dec, "rem :", (dec % 2))
+
+        dec = Math.floor((dec / 2));
+
+
+    }
+    while (dec >= 1)
+
+    console.log("full array :", ...remarray.reverse())
+    return remarray
+
+}
+
+// dectobin(156)
+// dectobin(200)
+dectobin(1000)
+
--- math/set2pdf/q21-primeupto.js
+++ math/set2pdf/q21-primeupto.js
@@ -7,7 +7,7 @@ Prime Number
 
 */
 
-//function IsPrime(num) {
+// function IsPrime(num) {
 //     console.log("hello")
 //     if (num < 2) { return false }
 //     if (num == 2 || num == 3) { return true }
@@ -18,14 +18,67 @@ Prime Number
 //             }
 //         }
 //         return true
-//     }   
-   
+//     }
+
 // }
 
-// console.log(IsPrime(97))
+// console.log(IsPrime())
 
 
 
-function UptoPrime(lmt){
-
+function ISPrime(n) {
+    console.log(n)
+    if (n < 2) return false;
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true
 }
+function upto(n) {
+    let primarr = []
+    for (j = 0; j <= n; j++) {
+        console.log()
+        if (ISPrime(j)) primarr.push(j)
+    }
+    console.log(...primarr)
+}
+console.log(upto(16));
+
+
+
+
+function testt(){
+ISPrime
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// psudo
+// prime number or not 
+// if(num<2)ret false
+// for(i=2;i<=Math.sqrt(num);i++){
+//     if(num%i==0) return false  , no else part
+// return true 
+// }
+
