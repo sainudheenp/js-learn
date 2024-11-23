049fc38ec6375971d92e9624851109b41247a580
sainudheenp <sainudheen2003@gmail.com>
2024-11-23 14:48:30 +0530

    stoped

--- math/set2pdf/q24-fraction.js
+++ math/set2pdf/q24-fraction.js
@@ -1,1 +1,14 @@
-console.log("")
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
+}
+
+function fr(numerator, denominator) {
+    const divisor = gcd(numerator, denominator);
+    return `${numerator / divisor}/${denominator / divisor}`;
+}
+
+console.log(fr(8, 12))
+
+// function test() {
+    
+// }
