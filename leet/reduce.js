cf0f751c232fb115d1c2acf4cdb25c38b068f989
sainudheenp <sainudheen2003@gmail.com>
2024-11-27 21:12:17 +0530

    las

---
+++ leet/reduce.js
@@ -0,0 +1,14 @@
+var reduce = function(nums, fn, init) {
+   return nums.reduce(fn)
+};
+
+
+// composedFunction
+
+const add =(a,b)=>a+b
+const mul=(a,b)=>a*b
+const sub =(a,b)=>a-b
+
+function composedFunction(add,mul,sum){
+
+}
