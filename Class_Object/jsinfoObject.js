0160abb3df9ef3100909abe0ddbaf7fa0b734e5a
sainudheenp <sainudheen2003@gmail.com>
2025-01-22 15:27:22 +0530

    .l

--- Class_Object/jsinfoObject.js
+++ Class_Object/jsinfoObject.js
@@ -1,5 +1,5 @@
-let user={}
-let cons_user = new Object ()
+let user = {}
+let cons_user = new Object()
 
 user.name = "Allen"
 user.surName = "Rintoul"
@@ -9,15 +9,34 @@ console.log(user)
 delete user.name
 console.log(user)
 
-let ref={
-    name:'sainu'
+let ref = {
+    name: 'sainu'
 }
 
 let ref2 = ref;
 console.log(ref)
 console.log(ref2)
-ref2.name="Sainu changed"
+ref2.name = "Sainu changed"
 
 console.log(ref)
 console.log(ref2)
+
+console.clear()
+
+console.log("InterLInk Objects");
+
+function marry(man, woman) {
+    woman.husband = man;
+    man.wife = woman;
+
+    return {
+        father: man,
+        mother: woman
+    }
+}
+
+let family = marry({name:"John"}, {name:"Ann"})
+
+console.log(family)
+
 
