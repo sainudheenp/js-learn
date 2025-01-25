27e303b23dd173d9b3690dd78788ea0d59534fe3
sainudheenp <sainudheen2003@gmail.com>
2025-01-25 13:49:15 +0530

    .

--- JS30/Css/script.js
+++ JS30/Css/script.js
@@ -1,11 +1,11 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-    console.log(this); // Logs the element that triggered the event
+    // console.log(this.name); 
     const suffix = this.getAttribute('data-sizing') || ''; // Get suffix if any (e.g., px for spacing/blur)
-    console.log(suffix);
-    // Update the corresponding CSS variable with the input's value and suffix
+    // console.log("suffix",suffix);
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    // console.log(    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix))
 }
 
 // Add event listeners once for all the inputs (outside handleUpdate)
