e3b7015de8dff10ecc23e0289892a6256a2a9fa7
sainudheenp <sainudheen2003@gmail.com>
2025-03-03 16:49:00 +0530

    des

---
+++ ForInt/closure2.js
@@ -0,0 +1,11 @@
+var objs =[
+    {name:'One',msg:'Hello I am one'},
+    {name:'two',msg:'Hello I am two'},
+    {name:'three',msg:'Hello I am three'}
+]
+
+function main(){
+objs.forEach(item=>{
+})
+
+}
