7d43cd4476ba57e5c1e7475b03dbcbdf0d6feed3
sainudheenp <sainudheen2003@gmail.com>
2025-03-05 16:44:24 +0530

    Add array manipulation examples to ArrayAdv.js

--- ForInt/ArrayAdv.js
+++ ForInt/ArrayAdv.js
@@ -1,3 +1,33 @@
 // https://davitdvalashvili1996.medium.com/javascript-array-methods-your-complete-guide-372b9c6f12cd
 
-const array = ["John","Jake","Ann"]
+const array = ["John", "Jake", "Ann"]
+
+console.log(array.pop())
+console.log(array)
+
+console.log(array.push("Meln"))
+console.log(array)
+
+console.log(array.shift())
+console.log(array)
+
+console.log(array.unshift("Lopz"))
+console.log(array)
+
+console.log(array.splice(2))
+console.log(array)
+
+console.log(array.slice(0, 2))
+
+
+// 
+console.log("////////////////////")
+
+console.log(array.concat(['3','5']))
+
+console.log(array.fill("fill",0,1))
+console.log(array)
+
+
+let str = "dfghjkl"
+console.log(array.split(" ").join('/'))
