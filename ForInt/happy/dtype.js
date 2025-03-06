5f54275946664949fdf389cb40f3cb5c726090b4
sainudheenp <sainudheen2003@gmail.com>
2025-03-06 16:54:40 +0530

    25 js vdo 17:00

---
+++ ForInt/happy/dtype.js
@@ -0,0 +1,29 @@
+let perosn = {
+    name: "John",
+    age: 40,
+    gen: "x"
+}
+
+const anim = ["Dog","Cat"]
+anim.push("Cow")
+
+for(let val of anim){
+    console.log(val)
+}
+
+/*
+
+var scoped access
+
+let block scoped
+
+while 
+do while 
+for 
+
+
+
+
+
+
+*/
